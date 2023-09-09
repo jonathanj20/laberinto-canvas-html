5e63d2321e433cb96ed700906fbd1f90e717209e
jonathanj20 <jonathanpossada@gmail.com>
2023-09-09 00:11:35 -0700

    se terminaron de crear todos los muros, se añadió una meta y un sonido al llegar, puntaje y tiempo

--- laberinto.js
+++ laberinto.js
@@ -1,13 +1,17 @@
 const canvas = document.getElementById('miCanvas');
 const ctx = canvas.getContext('2d');
 let posicionX = 15, posicionY = 15, oldX=0, oldY=0, w = 30, h = 30;
-let toco = false;
-let arregloMuros = [];
-let tucan = new Image();
+let toco = false, gano = false, tocoFresa = false;
+let arregloMuros = [], arregloFrutas =  [];
+let tucan = new Image(), fresa = new Image();
 const musica = document.getElementById('musicaAmbiental');
+const sonidoMeta = document.getElementById('sonidoFinal');
+let tiempoTotalSegundos = 0, puntuacion = 0;
 
 tucan.src = 'iconos/feliz.png'
+fresa.src = 'iconos/fresa.png'
 
+//clase para crear muros
 class Muro {
     constructor(x, y, ancho, alto){
         this.x = x;
@@ -31,11 +35,40 @@ class Muro {
     }
 }
 
+//clase para agregar frutas
+class Fresa {
+    constructor(x, y, ancho, alto){
+        this.x = x;
+        this.y = y;
+        this.ancho = ancho;
+        this.alto = alto;
+    }
+
+    dibujarFresa(){
+        ctx.drawImage(fresa, this.x, this.y, this.ancho, this.alto);
+    }
+
+    estaTocandoFresa(){
+        let t_x = this.x, t_y = this.y, t_w = this.ancho, t_h = this.alto;
+        if(posicionX < t_x + t_w && posicionX + w > t_x && posicionY < t_y + t_h && posicionY + h > t_y){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
 /*esta función crea un objeto de tipo Muro con las propiedades que le
 pasamos como parametros, y después los agrega al array*/
 function crearMuro(x, y, ancho, alto){
     const muro = new Muro(x, y, ancho, alto);
     arregloMuros.push(muro);
+}
+
+/**función para crear fresas y agregarlas al array*/
+function crearFresa(x, y, ancho, alto){
+    const fresa1 = new Fresa(x, y, ancho, alto);
+    arregloFrutas.push(fresa1);
 }
 
 //creación de muros
@@ -98,14 +131,40 @@ crearMuro(45, 261, 20, 10);
 crearMuro(10, 360, 35, 10);
 crearMuro(0, 500, 80, 10);
 crearMuro(40, 450, 10, 50);
-crearMuro(40, 440, 90, 10);
+crearMuro(40, 440, 150, 10);
+crearMuro(180, 410, 10, 40);
+crearMuro(180, 410, 100, 10);
+crearMuro(270, 410, 10, 50);
+crearMuro(330, 405, 100, 10); //lado donde puede estar el camino alterno
+crearMuro(330, 405, 10, 105);
+crearMuro(140, 500, 200, 10);
+crearMuro(230, 475, 10, 25);
+crearMuro(375, 470, 10, 90);
+crearMuro(375, 460, 80, 10);
+crearMuro(450, 460, 10, 50);
+crearMuro(450, 505, 50, 10);
+crearMuro(415, 525, 10, 25);
+crearMuro(420, 315, 10, 90);
+crearMuro(420, 315, 120, 10);
+crearMuro(525, 465, 65, 10);
+crearMuro(525, 370, 10, 100);
+crearMuro(525, 370, 65, 10);
+crearMuro(425, 370, 55, 10);
+crearMuro(480, 420, 45, 10);
+crearMuro(90, 400, 10, 40);
+crearMuro(390, 260, 10, 25);
+crearMuro(450, 295, 10, 25);
+crearMuro(510, 260, 10, 25);
+crearMuro(190, 500, 10, 20);
+crearMuro(345, 325, 20, 10);
+crearMuro(405, 360, 20, 10);
 
-pintarMapa();
-
-//Jugador inicial   
-tucan.onload = () => {
-    ctx.drawImage(tucan, posicionX, posicionY, w, h);
-}
+//creación de frutas
+crearFresa(65, 15, 30, 30);
+crearFresa(405, 15, 30, 30);
+crearFresa(245, 115, 30, 30);
+crearFresa(490, 200, 30, 30);
+crearFresa(5, 455, 30, 30);
 
 //movimiento del jugador
 document.addEventListener("keydown",(e) => {
@@ -124,7 +183,8 @@ document.addEventListener("keydown",(e) => {
             break;
     }
     
-    verificarColision();
+    verificarColisionMuro();
+    verificarColisionFresa();
     
     //si lo tocó, entonces se reinicia de nuevo la variable tocó. 
     if(!toco){
@@ -136,6 +196,7 @@ document.addEventListener("keydown",(e) => {
 
     pintarMapa();
     dibujarTucan();
+    llegoMeta();
 });
 
 //esta función dibuja el personaje
@@ -145,18 +206,18 @@ function dibujarTucan(){
 
 function pintarMapa(){
     ctx.fillStyle = '#6ed671';
-    ctx.fillRect(0, 0, 600, 600);
+    ctx.fillRect(0, 0, 600, 560);
 
     for(let i = 0; i < arregloMuros.length; i++){
         arregloMuros[i].pintarMuro();
     }
 
-    //parte inferior para puntajes
-    ctx.fillStyle = '#fff';
-    ctx.fillRect(0, 560, 600, 40);
+     for(let i = 0; i < arregloFrutas.length; i++){
+        arregloFrutas[i].dibujarFresa();
+    }
 }
 
-function verificarColision(){
+function verificarColisionMuro(){
     for(let i = 0; i < arregloMuros.length; i++){
         if(arregloMuros[i].estaTocando()){ 
             posicionX = oldX;
@@ -167,12 +228,83 @@ function verificarColision(){
     }
 }
 
+function verificarColisionFresa(){
+    for(let i = 0; i < arregloFrutas.length; i++){
+        for (let i = 0; i < arregloFrutas.length; i++) {
+            if (arregloFrutas[i].estaTocandoFresa()) {
+                console.log('tocó la fruta');
+                arregloFrutas.splice(i, 1); // Elimina la fruta del arreglo
+                puntuacion++;
+                break;
+            }
+        }
+    }
+}
+
+function llegoMeta(){
+    let t_x = 540, t_y = 500, t_w = 50, t_h = 50;
+
+    ctx.fillStyle = '#fff';
+    ctx.fillRect(t_x, t_y, t_w, t_h);
+
+    if(posicionX < t_x + t_w && posicionX + w > t_x && posicionY < t_y + t_h && posicionY + h > t_y){
+        console.log('llegaste a la METAA!!!')
+        sonidoMeta.play();
+        gano = true;
+    }
+}
+
+function puntaje(){
+    ctx.fillStyle = '#000';
+    ctx.font = '20px Arial';
+    ctx.fillText(`Puntos: ${puntuacion}`, 400, 585);
+}
+
 //este evento se está usando para reproducir la música cuando cargue la página
 window.addEventListener('load', () => {
     musica.play();
+    pintarMapa();
+    dibujarTucan()
+    llegoMeta();
 });
 
 //este evento se está usando para reproducir la música de nuevo cuando termine
 musica.addEventListener('ended', () => {
-    musica.play()
+    musica.play();
+});
+
+//cronómetro
+// Función para formatear el tiempo en minutos y segundos
+function formatearTiempo(tiempo) {
+    const minutos = Math.floor(tiempo / 60);
+    const segundos = tiempo % 60;
+    const minutosFormateados = minutos < 10 ? `0${minutos}` : minutos;
+    const segundosFormateados = segundos < 10 ? `0${segundos}` : segundos;
+    return `${minutosFormateados}:${segundosFormateados}`;
+}
+
+//0, 560 coordenadas del rectangulo, y 600 de ancho
+// Función para actualizar el cronómetro y dibujarlo en el canvas
+function actualizarCronometro() {
+    if(gano){
+        ctx.fillStyle = 'rgba(255, 255, 255, 0.7)';
+        ctx.fillRect(0, 560, canvas.width, 40); // Fondo del cronómetro
+        ctx.font = '20px Arial';
+        ctx.fillStyle = 'red';
+        ctx.fillText(`Tiempo: ${formatearTiempo(tiempoTotalSegundos)}`, 10, 585);
+    } else {
+        tiempoTotalSegundos++;
+        ctx.clearRect(0, 560, canvas.width, 40); // Limpiar el área del cronómetro
+        ctx.fillStyle = 'rgba(255, 255, 255, 0.7)';
+        ctx.fillRect(0, 560, canvas.width, 40); // Fondo del cronómetro
+        ctx.font = '20px Arial';
+        ctx.fillStyle = 'black';
+        ctx.fillText(`Tiempo: ${formatearTiempo(tiempoTotalSegundos)}`, 10, 585);
+        puntaje();
+    }
+}
+
+// Iniciar el cronómetro cuando se cargue la página
+window.addEventListener('load', () => {
+    setInterval(actualizarCronometro, 1000); // Actualiza cada segundo
 });
